73b7a1753e0624ff972af4f07484eacbe2bd5e04
Shavar <shavar.litchmore@ecsazza.com>
2025-02-04 21:31:01 -0500

    Update TwoSums.java

--- src/main/java/random/TwoSums.java
+++ src/main/java/random/TwoSums.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class TwoSums {
     /*
-    iven an array of integers, return indices of the two numbers such that they add up to a specific target.
+    given an array of integers, return indices of the two numbers such that they add up to a specific target.
     You may assume that each input would have exactly one solution, and you may not use the same element twice.
     Example:
     Given nums = [2, 7, 11, 15], target = 9,
